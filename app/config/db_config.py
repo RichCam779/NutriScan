4ab5897e68390e5badedcbc6ccdf7cde7d959715
RichCam <richcam277@gmail.com>
2026-02-08 05:06:50 -0500

    Configuración para despliegue en Vercel

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,10 +1,20 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_connection():
+    # Vercel usará esta variable que configuraremos en su panel
+    url = os.getenv("DATABASE_URL")
+    if url:
+        return psycopg2.connect(url)
+    
+    # Esto es solo por si algo falla, pero lo ideal es usar la URL arriba
     return psycopg2.connect(
-        host="ep-twilight-band-aijnd7uk-pooler.c-4.us-east-1.aws.neon.tech",
-        port="5432",
-        user="neondb_owner",
-        password="npg_KW7YPyI9SOQq",
-        dbname="neondbs"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port="5432"
     )
